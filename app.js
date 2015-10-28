083cee6a82a691e8d6a1ec512ca44e0eba5253a8
beforexmas <raoul.sbarberi@gmail.com>
2015-10-28 17:44:29 +0100

    Top

--- app.js
+++ app.js
@@ -6,8 +6,30 @@
 // hidden: when the card has not been discovered yet
 
 $(document).ready(function(){
-
-  
+    var previours_number=0;
+    for (var i=0;i<data.length;i++) {
+        $('.cards').append("<li id='1' class='done'><h3>num</h3></li>  ".replace("num",data[i]));    
+    }
+    $('.opt-start').click(function() {
+        $('li').each(function() {
+            $('li').removeClass('done');
+            $('li').addClass('hidden');                 
+        });
+    });
+    $('li').click(function() {
+        $(this).removeClass('hidden');
+        $(this).addClass('done');
+        if ($(this).text()==previours_number+1) {
+            
+        }
+        else {
+            alert('You lose');   
+        }
+        if ($(this).text()==data.length) {
+            alert('You won');    
+        }
+        previours_number++;
+    });
 });
 
 
